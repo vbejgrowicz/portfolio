fcb9cc946ff0fa2b63d9b8ddcdd732870a833525
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-13 17:48:05 -0500

    Add video with play on open to blockbreaker project modal

--- js/freelancer.js
+++ js/freelancer.js
@@ -61,15 +61,11 @@
     $.magnificPopup.close();
   });
 
-  // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-      $(this).removeClass("floating-label-form-group-with-focus");
-    });
-  });
+  $('.video-modal-open').click(function () {
+    $('#video')[0].play();
+  })
+  $('.video-modal-close').click(function () {
+    $('#video')[0].pause();
+  })
 
 })(jQuery); // End of use strict
